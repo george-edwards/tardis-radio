43cb8583c52c0f51312fb98eb9fff54d9658b69c
George Edwards <georgeedwardsmail@gmail.com>
2021-10-27 15:55:02 +1100

    installation script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v
+echo "installing pre-requisites"
+sudo apt install sox libsox-fmt-all bc -y
+
+echo "installing systemd daemons"
+sudo cp systemd-files/* /lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable low-power-daemon.service
+sudo systemctl enable power-switch-daemon.service
+sudo systemctl enable radio.service
+sudo systemctl enable volume-daemon.service
