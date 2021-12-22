b9faa666cddda09a82932ccafca75778d3b45c2d
George Edwards <georgeedwardsmail@gmail.com>
2021-12-23 07:05:54 +1100

    added face plate and changed systemd daemon names

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt install sox libsox-fmt-all bc -y
 echo "installing systemd daemons"
 sudo cp systemd-files/* /lib/systemd/system/
 sudo systemctl daemon-reload
-#sudo systemctl enable low-power-daemon.service
-#sudo systemctl enable power-switch-daemon.service
-sudo systemctl enable radio.service
-sudo systemctl enable volume-daemon.service
+#sudo systemctl enable tardis-radio-low-power-daemon.service
+#sudo systemctl enable tardis-radio-power-switch-daemon.service
+sudo systemctl enable tardis-radio.service
+sudo systemctl enable tardis-radio-volume-daemon.service
 
 echo "configuring sound device for soft-volume"
 cp .asoundrc /home/pi/
